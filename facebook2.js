a5c6ddfdabd557c34b183d6afe44615bc0ee229a
Syed Ragib Israq <ragib.israq9833@gmail.com>
2022-10-18 13:55:16 +0600

    using forloop 18-10-22

--- facebook2.js
+++ facebook2.js
@@ -2,6 +2,14 @@ var database = [
     {
         username: "israq",
         password: "israq127"
+    },
+    {
+        username: "sally",
+        password: "123"
+    },
+    {
+        username: "ingrid",
+        password: "777"
     }
 ];
 var newsFeed = [
@@ -17,14 +25,29 @@ var newsFeed = [
 
 var userNamePrompt = prompt("Username Please");
 var passwordPrompt = prompt("Password Please");
-
+function isUserValid(user, pass) {
+    for(var i = 0; i < database.length; i++){
+        if(database[i].username === user && 
+            database[i].password === pass) {
+                return true;
+            } 
+        }
+    return false;
+}
 function logIn(user ,pass) {
-    if (user === database[0].username &&
-        pass === database[0].password) {
+    if (isUserValid(user, pass)) {
         console.log(newsFeed);
     } else {
-        alert("Sorry login info doesn't match");
+        alert("sorry wrong pass / user");
     }
+    }
+    
+    //if (user === database[0].username &&
+    //    pass === database[0].password) {
+    //    console.log(newsFeed);
+    //} else {
+    //   alert("Sorry login info doesn't match");
+    //}
         
-}
+
 logIn(userNamePrompt, passwordPrompt);
